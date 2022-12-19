4f2e7a5a892742c3dd66eede95829837192d8bef
Vedavyasy <yvedavyas4077@gmail.com>
2022-12-19 14:14:59 +0530

    retrevie repo data

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { getInput } from "@actions/core";
+import { context  } from "@actions/github";
 
+type GithubContext = typeof context;
 const inputName = getInput("name");
 
-greet(inputName);
+greet(inputName,getrepourl(context));
 
-function greet(name: string) {
-  console.log(`'Hello ${name}!`);
+function greet(name: string, repoUrl: string) {
+    console.log(`'Hello ${name}! You are running a GH Action in ${repoUrl}'`);
+  }
+
+function getrepourl({repo, serverUrl} : GithubContext): string {
+   return '${serverUrl}/${repo.owner}/${repo.repo}'
 }
