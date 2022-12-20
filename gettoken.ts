dfbdba9de2245fe940f0f3f4ae38f3932cb85829
Vedavyasy <yvedavyas4077@gmail.com>
2022-12-20 14:29:37 +0530

    test

--- gettoken.ts
+++ gettoken.ts
@@ -38,14 +38,18 @@ export const fetchInstallationToken = async ({ appId,
     const authApp = await app({ type: "app" });
     const octokit = getOctokit(authApp.token);
     console.log(await octokit.rest.apps.getRepoInstallation({ owner, repo }))
-    
-    const installation = await app({
-        installationId,
-        permissions,
-        type: "installation",
-    });
-    console.log(`'Hello'`);
-    return installation.token;
+    if (installationId == undefined) {
+        const authApp = await app({ type: "app" });
+        const octokit = getOctokit(authApp.token);
+        try {
+            ({
+                data: { id: installationId },
+            } = await octokit.rest.apps.getRepoInstallation({ owner, repo }));
+        } catch (error) {
+            throw new Error("Could not get the repo installation. is the app installed on this repo?");
+        }
+    }
+    return "requested token";
 };
 console.log(fetchInstallationToken({ appId: "272308",
 owner: context.repo.owner,
