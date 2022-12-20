80cc0c0cb0e4f3c1e91c4f2530481d770dcff797
Vedavyasy <yvedavyas4077@gmail.com>
2022-12-20 13:53:01 +0530

    Private key rendering

---
+++ Git token dummy.ts
@@ -0,0 +1,64 @@
+import { getInput } from "@actions/core";
+import { context ,getOctokit } from "@actions/github";
+import { createAppAuth } from '@octokit/auth-app';
+import { request } from '@octokit/request';
+import { report } from "process";
+
+const inputName = getInput("name");
+greet(inputName);
+function greet(name: string) {
+    console.log(`'Hello ${name}'`);
+}
+export const fetchInstallationToken = async ({ appId,
+                                                installationId,
+                                                owner,
+                                                permissions,
+                                                privateKey,
+                                                repo,
+                                                apiUrl,
+    }: Readonly<{
+        appId: string;
+        installationId?: number;
+        owner: string;
+        permissions?: Record<string, string>;
+        privateKey: string;
+        repo: string;
+        apiUrl: string;
+    }>): Promise<string> => {
+        const app = createAppAuth({
+            appId,
+            privateKey,
+            request: request.defaults({
+                baseUrl: apiUrl,
+            }),
+        });
+        if (installationId == undefined) {
+            const authApp = await app({ type: "app" });
+            const octokit = getOctokit(authApp.token);
+            try {
+                ({
+                    data: { id: installationId },
+
+                } = await octokit.rest.apps.getRepoInstallation({ owner, repo }));
+            } catch (error) {
+                throw new Error("Could not get the repo installation. is the app installed on this repo?");
+            }
+        }
+        const installation = await app({
+            installationId,
+            permissions,
+            type: "installation",
+        });
+        console.log(`'Hello'`);
+        return installation.token;
+    };
+
+var tokarg={
+    appId:"272308",
+    installationId: 2545,
+    owne: context.repo.owner,
+    privateKey: './token.pem',
+    repo: context.repo.repo,
+    apiUrl :" ",
+}
+fetchInstallationToken(tokarg);
--- gettoken.ts
+++ gettoken.ts
@@ -1,53 +1,20 @@
 import { getInput } from "@actions/core";
-import { getOctokit } from "@actions/github";
+import { context ,getOctokit } from "@actions/github";
 import { createAppAuth } from '@octokit/auth-app';
 import { request } from '@octokit/request';
+import { report } from "process";
 
 const inputName = getInput("name");
 greet(inputName);
 function greet(name: string) {
     console.log(`'Hello ${name}'`);
 }
-export const fetchInstallationToken = async ({ appId,
-                                                installationId,
-                                                owner,
-                                                permissions,
-                                                privateKey,
-                                                repo,
-                                                apiUrl,
-    }: Readonly<{
-        appId: string;
-        installationId?: number;
-        owner: string;
-        permissions?: Record<string, string>;
-        privateKey: string;
-        repo: string;
-        apiUrl: string;
-    }>): Promise<string> => {
-        const app = createAppAuth({
-            appId,
-            privateKey,
-            request: request.defaults({
-                baseUrl: apiUrl,
-            }),
-        });
-        if (installationId == undefined) {
-            const authApp = await app({ type: "app" });
-            const octokit = getOctokit(authApp.token);
-            try {
-                ({
-                    data: { id: installationId },
 
-                } = await octokit.rest.apps.getRepoInstallation({ owner, repo }));
-            } catch (error) {
-                throw new Error("Could not get the repo installation. is the app installed on this repo?");
-            }
-        }
-        const installation = await app({
-            installationId,
-            permissions,
-            type: "installation",
-        });
-        console.log(`'Hello'`);
-        return installation.token;
-    };
+const fs = require("fs");
+var myKey = fs.readFileSync("./token.pem", "utf8").replace("-----BEGIN RSA PRIVATE KEY-----", "").replace("-----END RSA PRIVATE KEY-----", "").trim();
+console.log("My key is: ", myKey);
+
+
+function gettoken(appId:string, privateKey: string){
+
+}
