1e735220faf6691710c799c3a5b60148926612ba
Vedavyasy <yvedavyas4077@gmail.com>
2022-12-20 14:14:43 +0530

    Updated get token

--- gettoken.ts
+++ gettoken.ts
@@ -1,5 +1,5 @@
 import { getInput } from "@actions/core";
-import { context ,getOctokit } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import { createAppAuth } from '@octokit/auth-app';
 import { request } from '@octokit/request';
 import { report } from "process";
@@ -9,19 +9,56 @@ greet(inputName);
 function greet(name: string) {
     console.log(`'Hello ${name}'`);
 }
-
 const fs = require("fs");
 var myKey = fs.readFileSync("./token.pem", "utf8").replace("-----BEGIN RSA PRIVATE KEY-----", "").replace("-----END RSA PRIVATE KEY-----", "").trim();
-console.log("My key is: ", myKey);
-gettoken("272308",myKey)
 
-function gettoken(appId:string, privateKey: string){
+export const fetchInstallationToken = async ({ appId,
+    installationId,
+    owner,
+    permissions,
+    privateKey,
+    repo,
+    apiUrl,
+}: Readonly<{
+    appId: string;
+    installationId?: number;
+    owner: string;
+    permissions?: Record<string, string>;
+    privateKey: string;
+    repo: string;
+    apiUrl: string;
+}>): Promise<string> => {
     const app = createAppAuth({
         appId,
         privateKey,
         request: request.defaults({
-            baseUrl: "https://Githubapptest/api/v3",
+            baseUrl: apiUrl,
         }),
     });
-    console.log(app);
-}
+    if (installationId == undefined) {
+        const authApp = await app({ type: "app" });
+        const octokit = getOctokit(authApp.token);
+        try {
+            ({
+                data: { id: installationId },
+
+            } = await octokit.rest.apps.getRepoInstallation({ owner, repo }));
+        } catch (error) {
+            throw new Error("Could not get the repo installation. is the app installed on this repo?");
+        }
+    }
+    const installation = await app({
+        installationId,
+        permissions,
+        type: "installation",
+    });
+    console.log(`'Hello'`);
+    return installation.token;
+};
+console.log(fetchInstallationToken({ appId: "272308",
+installationId: 245,
+owner: context.repo.owner,
+privateKey: myKey,
+repo:"dummy",
+apiUrl:"https://Githubapptest/api/v3",
+}))
